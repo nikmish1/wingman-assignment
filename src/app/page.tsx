73b09836a9466df03ae3e3cb6012a57018746a5f
Nikhil Mishra <nikhil15.1990@gmail.com>
2024-12-23 01:26:49 +0530

    feat: integrate useFetchOrders hook in Dashboard and Orders components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import useFetchOrders from "@/hooks/useFetchOrders";
 import { Dashboard } from "./Dashboard";
 
 export default function Home() {
-  const { data, loading } = useFetchOrders();
-
-  return loading ? <div>Loading...</div> : <Dashboard orders={data as []} />;
+  return <Dashboard />;
 }
